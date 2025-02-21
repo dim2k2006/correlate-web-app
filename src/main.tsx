7d856eda37b6332f9165ab08cc8f99e346148419
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-02-21 18:19:16 +0100

    Update main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './components/app/app.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import ConfigProvider from './components/config-provider';
+import { ParameterServiceProvider } from './components/parameter-service-provider';
+import { buildConfig, buildContainer } from './container';
+
+const queryClient = new QueryClient();
+
+const config = buildConfig();
+const container = buildContainer(config);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ConfigProvider config={config}>
+        <ParameterServiceProvider service={container.parameterService}>
+          <App />
+        </ParameterServiceProvider>
+      </ConfigProvider>
+    </QueryClientProvider>
   </StrictMode>,
-)
+);
